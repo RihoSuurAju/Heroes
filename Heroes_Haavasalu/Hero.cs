c338c4eb44741908a09b60846077eb21ebd72934
Riho <rihomartin.haavasalu@gmail.com>
2019-11-15 09:15:47 +0200

    they gon save us

---
+++ Heroes_Haavasalu/Hero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes_Haavasalu
+{
+	class Hero
+	{
+		private string name;
+		private string location;
+
+		public Hero(string name, string location)
+		{
+			this.name = name;
+			this.location = location;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public string Location { get => location; set => location = value; }
+
+		public virtual int Rescue(int amountOfPeople)
+		{
+			return (int)Math.Round(amountOfPeople * 0.95);
+		}
+
+		public override string ToString()
+		{
+			return $"Hello! My name is {name}, and I come from {location}";
+		}
+	}
+
+	class Superhero : Hero
+	{
+		double skill;
+
+		public Superhero(string name, string location) : base(name, location)
+		{
+			Random r = new Random();
+			skill = r.Next(1, 5);
+		}
+
+		public override string ToString()
+		{
+			return $"Superhero: {Name} \nLocation: {Location} \nSkill: {skill}";
+		}
+
+		public override int Rescue(int amountofPeople)
+		{
+			return (int)Math.Round(amountofPeople * 0.95 + skill);
+		}
+
+	}
+}
---
+++ Heroes_Haavasalu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes_Haavasalu
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random r = new Random();
+
+			Hero hero = new Hero("nunya", "Earth");
+			Console.WriteLine("His name is " + hero.Name);
+			Console.WriteLine("He resides within " + hero.Location);
+			int rescued = hero.Rescue(r.Next(1,1000000));
+			Console.WriteLine(hero + $"\nThis hero has saved {rescued} people.");
+			Console.WriteLine();
+
+			Superhero superhero = new Superhero("Jimmy Neutron", "Retroville");
+			int superRescued = superhero.Rescue(100);
+			Console.WriteLine(superhero + $"\nThis superhero has rescued {superRescued} people.");
+			Console.ReadLine();
+		}
+	}
+}
